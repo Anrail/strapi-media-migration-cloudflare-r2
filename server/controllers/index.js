f9b5b4de48f3c441a78a3000e504621bbd6b6c55
iartemii <artemii@thefirstthelast.agency>
2025-11-28 17:12:56 +0100

    added reupload controller to the index

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,8 +3,10 @@
 
 const migration = require('./migration');
 const changeDomain = require('./changeDomain');
+const reupload = require('./reupload')
 
 module.exports = {
   migration,
   changeDomain,
+  reupload
 };
