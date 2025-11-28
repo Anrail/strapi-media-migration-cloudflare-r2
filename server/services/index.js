d43ee0574b3c5bc0b7288be02d3491368c4cae05
iartemii <artemii@thefirstthelast.agency>
2025-11-28 17:41:02 +0100

    added reupload service to the index

--- server/services/index.js
+++ server/services/index.js
@@ -2,8 +2,10 @@
 
 const migration = require('./migration');
 const changeDomain = require('./changeDomain');
+const reupload = require('./reupload')
 
 module.exports = {
   migration,
   changeDomain,
+  reupload
 };
